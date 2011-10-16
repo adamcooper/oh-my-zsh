c2076bd4253dca0fe3e3dc3cd178246c55b4aad1
Adam Cooper <adam.cooper@gmail.com>
2011-10-16 16:34:29 -0700

    couple aliases and exports

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -1,3 +1,7 @@
 alias tm='mate app config db/migrate lib spec features public/javascripts'
 alias t='python /usr/local/Cellar/t/t.py --task-dir ~/tasks --list tasks'
+alias b='bundle exec'
+export SEED_DATA=/Users/adam/Dropbox/work/cisco/test_seed_data
+export APK_SEED_DATA=/Users/adam/Dropbox/work/cisco/device_apks
+
 
